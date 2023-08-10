c1847cfeb5df238027342f5234939b1443957186
Mike Orr <mike.orr79@gmail.com>
2023-08-09 19:25:05 -0700

    Accepted answer for Project Euler #2

---
+++ ProjectEuler/ProjectEuler2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+typedef unsigned long long ull;
+ull compute(ull n) {
+    ull fib1 = 1;
+    ull fib2 = 2;
+
+    ull total = 0; 
+    while(fib2 < n) {
+        if (fib2 % 2LL == 0LL) {
+            total += fib2;
+        } 
+        ull fib3 = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = fib3;
+    }
+    return total;
+}
+
+int main(){
+ 
+    int t;
+    cin >> t;
+    for(int a0 = 0; a0 < t; a0++){
+        ull n;
+        cin >> n;
+        cout << compute(n) << endl;   
+    }
+    return 0;
+}
